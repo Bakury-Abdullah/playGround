40147768d3c2d7be6c5e42746189ba3312012fcb
[Abdullah] <[abdallahelbakury@gmail.com]>
2025-05-27 16:17:17 +0300

    advancing the busCounterApp

--- busCounterApp/busCounterApp.js
+++ busCounterApp/busCounterApp.js
@@ -2,10 +2,17 @@
 let count = 0;
 
 let Busses = document.getElementById("count-el")
+let saveBtn = document.getElementById("save-btn")
+let saved = document.getElementById("saved-count")
 
 function counter(){
-    count = count + 1;
+    count += 1;
     Busses.innerText = count;
-    console.log(count);
 }
 
+function save(){
+    let countStr = count + " - ";
+    saved.textContent += countStr;
+    count = 0;
+    Busses.innerText = count;
+}
--- scrimbaCopying/copying.js
+++ scrimbaCopying/copying.js
@@ -1,6 +1,21 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+
+
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
 function increment() {
-    console.log("The button was clicked")
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countstr = count + " - "
+    saveEl.textContent += countstr
+    console.log(count)
+    count = 0
+    countEl.innerText = count
 }
 
 
- 
